e883c3d048e4c26f7b963430792b8bf5f06a790c
Jossie <jossiemathews@gmail.com>
2014-03-25 10:48:37 +0530

    side a and B are added

--- js/playList.js
+++ js/playList.js
@@ -52,19 +52,19 @@ define([
                     cssSelectorAncestor: "#jp_container_1"
                 },[{    title:"Crazy Loud",
                         free: false,
-                        mp3: "./music/side-a/crazy_loud.mp3",
+                        mp3: "https://s3.amazonaws.com/coreyjon/music/side-a/crazy_loud.mp3",
                         oga: "./music/side-a/crazy_loud.oga"
                     }, {title:"One Minute",
                         free: false,
-                        mp3: "./music/side-a/one_minute.mp3",
+                        mp3: "https://s3.amazonaws.com/coreyjon/music/side-a/one_minute.mp3",
                         oga: "./music/side-a/one_minute.oga"
                     }, {title:"Renegade",
                         free: false,
-                        mp3: "./music/side-a/renegade.mp3",
+                        mp3: "https://s3.amazonaws.com/coreyjon/music/side-a/renegade.mp3",
                         oga: "./music/side-a/renegade.oga"
                     }, {title:"Out of Control",
                         free: false,
-                        mp3: "./music/side-a/out_of_control_2175.mp3",
+                        mp3: "https://s3.amazonaws.com/coreyjon/music/side-a/out_of_control_2175.mp3",
                         oga: "./music/side-a/out_of_control_2175.oga"
                     }],
                 playlistOptions);
@@ -74,19 +74,19 @@ define([
                     cssSelectorAncestor: "#jp_container_2"
                 },[{    title:"Crazy Glue",
                         free: false,
-                        mp3: "./music/side-b/crazy_glue.mp3",
+                        mp3: "https://s3.amazonaws.com/coreyjon/music/side-b/crazy_glue.mp3",
                         oga: "./music/side-b/crazy_glue.oga"
                     }, {title:"Long Stroke",
                         free: false,
-                        mp3: "./music/side-b/long_stroke.mp3",
+                        mp3: "https://s3.amazonaws.com/coreyjon/music/side-b/long_stroke.mp3",
                         oga: "./music/side-b/long_stroke.oga"
                     }, {title:"Matters",
                         free: false,
-                        mp3: "./music/side-b/matters.mp3",
+                        mp3: "https://s3.amazonaws.com/coreyjon/music/side-b/matters.mp3",
                         oga: "./music/side-b/matters.oga"
                     }, {title:"Got to Have You",
                         free: false,
-                        mp3: "./music/side-b/got_to_have_you.mp3",
+                        mp3: "https://s3.amazonaws.com/coreyjon/music/side-b/got_to_have_you.mp3",
                         oga: "./music/side-b/got_to_have_you.oga"
                     }],
                 playlistOptions);
